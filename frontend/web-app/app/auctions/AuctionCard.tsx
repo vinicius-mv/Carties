8737d60fb56b6daa118a0280abc95dc795449a78
“Vinicius <viniciusmv91@gmail.com>
2024-08-31 19:10:07 -0300

    Add load state to the auction image

--- frontend/web-app/app/auctions/AuctionCard.tsx
+++ frontend/web-app/app/auctions/AuctionCard.tsx
@@ -1,23 +1,17 @@
-import Image from 'next/image'
+
 import React from 'react'
 import CountdownTimer from './CountdownTimer'
+import CarImage from './CarImage';
 
 type Props = {
-    auction: any
+    auction: any;
 }
 
 export default function AuctionCard({ auction }: Props) {
     return (
-        <a href='#'>
+        <a href='#' className='group'>
             <div className='relative w-full bg-gray-200 aspect-[16/10] rounded-lg overflow-hidden'>
-                <Image
-                    src={auction.imageUrl}
-                    alt={`Image of ${auction.make} ${auction.model} in ${auction.color}`}
-                    fill
-                    priority
-                    className='object-cover'
-                    sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 25vw'
-                />
+                <CarImage imageUrl={auction.imageUrl} />
                 <div className='absolute bottom-2 left-2'>
                     <CountdownTimer auctionEnd={auction.auctionEnd} />
                 </div>
