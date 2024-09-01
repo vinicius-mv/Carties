b5a713414c0587673d8650477e8d3616c3848c8c
“Vinicius <viniciusmv91@gmail.com>
2024-08-31 22:46:32 -0300

    Add a set page size option

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -5,6 +5,7 @@ import AuctionCard from './AuctionCard';
 import AppPagination from '../components/AppPagination';
 import { Auction } from '@/types';
 import { getData } from '../actions/auctionActions';
+import Filters from './Filters';
 
 
 
@@ -12,18 +13,20 @@ export default function Listings() {
     const [auctions, setAuctions] = useState<Auction[]>([]);
     const [pageCount, setPageCount] = useState(0);
     const [pageNumber, setPageNumber] = useState(1);
+    const [pageSize, setPageSize] = useState(4);
 
     useEffect(() => {
-        getData(pageNumber).then(data => {
+        getData(pageNumber, pageSize).then(data => {
             setAuctions(data.results);
             setPageCount(data.pageCount);
         });
-    }, [pageNumber]);
+    }, [pageNumber, pageSize]);
 
     if (auctions.length === 0) return <h3>Loading...</h3>
 
     return (
         <>
+            <Filters pageSize={pageSize} setPageSize={setPageSize} />
             <div className='grid grid-cols-4 gap-6'>
                 {auctions.map(auction => (
                     < AuctionCard auction={auction} key={auction.id} />
