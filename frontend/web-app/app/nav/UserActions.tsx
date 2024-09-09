c0dd0bc2989a3019da492a345a6bd81a272f0b0d
“Vinicius <viniciusmv91@gmail.com>
2024-09-08 21:02:28 -0300

    Get the session details in the client

---
+++ frontend/web-app/app/nav/UserActions.tsx
@@ -0,0 +1,11 @@
+import { Button } from 'flowbite-react'
+import Link from 'next/link'
+import React from 'react'
+
+export default function UserActions() {
+    return (
+        < Button outline >
+            <Link href='/session'>Session</Link>
+        </Button >
+    )
+}
---
+++ frontend/web-app/app/session/page.tsx
@@ -0,0 +1,18 @@
+import { auth } from '@/auth';
+import React from 'react'
+import Heading from '../components/Heading';
+
+export default async function Session() {
+    const session = await auth();
+
+    return (
+        <div>
+            <Heading title='Session dashboard' />
+
+            <div className='bg-blue-200 border-2 border-blue-500'>
+                <h3 className='text-lg'>Session data</h3>
+                <pre>{JSON.stringify(session, null, 2)}</pre>
+            </div>
+        </div>
+    )
+}
