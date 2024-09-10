ae153770668ee6b0a8dd1ebc65dae0aedcaed624
“Vinicius <viniciusmv91@gmail.com>
2024-09-09 22:58:41 -0300

    Populate the User actions dropdown

--- frontend/web-app/app/nav/Logo.tsx
+++ frontend/web-app/app/nav/Logo.tsx
@@ -1,14 +1,23 @@
 'use client'
 
 import { useParamsStore } from '@/hooks/useParamsStore'
+import { usePathname, useRouter } from 'next/navigation'
 import React from 'react'
 import { AiOutlineCar } from 'react-icons/ai'
 
 export default function Logo() {
+    const router = useRouter();
+    const pathname = usePathname();
+
+    function doReset() {
+        if (pathname !== '/') router.push('/');
+        reset();
+    }
+
     const reset = useParamsStore(state => state.reset);
 
     return (
-        <div onClick={reset}
+        <div onClick={doReset}
             className='cursor-pointer flex items-center gap-2 text-3xl font-semibold text-red-500'>
             <AiOutlineCar />
             <div>Carsties Auctions</div>
--- frontend/web-app/app/nav/UserActions.tsx
+++ frontend/web-app/app/nav/UserActions.tsx
@@ -1,11 +1,48 @@
-import { Button } from 'flowbite-react'
-import Link from 'next/link'
-import React from 'react'
+'use client'
 
-export default function UserActions() {
+import { Dropdown, DropdownDivider, DropdownItem } from 'flowbite-react'
+import { User } from 'next-auth'
+import { signOut } from 'next-auth/react' // signOut for client side component. Do not use: import { signOut } from '@/auth', it is a client side function 
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import React from 'react'
+import { AiFillCar, AiFillTrophy, AiOutlineLogout } from 'react-icons/ai'
+import { HiUser } from 'react-icons/hi'
+import { HiCog } from 'react-icons/hi2'
+
+type Props = {
+    user: User
+}
+
+export default function UserActions({ user }: Props) {
+    const router = useRouter();
+
     return (
-        < Button outline >
-            <Link href='/session'>Session</Link>
-        </Button >
+        <Dropdown inline label={`Welcome ${user.name}`}>
+            <DropdownItem icon={HiUser}>
+                <Link href='/'>
+                    My Auctions
+                </Link>
+            </DropdownItem>
+            <DropdownItem icon={AiFillTrophy}>
+                <Link href='/'>
+                    Auctions won
+                </Link>
+            </DropdownItem>
+            <DropdownItem icon={AiFillCar}>
+                <Link href='/'>
+                    Sell my car
+                </Link>
+            </DropdownItem>
+            <DropdownItem icon={HiCog}>
+                <Link href='/session'>
+                    Session (dev only)
+                </Link>
+            </DropdownItem>
+            <DropdownDivider />
+            <DropdownItem icon={AiOutlineLogout} onClick={() => signOut({ callbackUrl: '/' })}>
+                Sign out
+            </DropdownItem>
+        </Dropdown>
     )
 }
