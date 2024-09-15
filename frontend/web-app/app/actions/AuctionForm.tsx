320b9bdcf11c8e73d10a72c540864b9705f0099a
“Vinicius <viniciusmv91@gmail.com>
2024-09-15 20:56:35 -0300

    Create a reusable text input

--- frontend/web-app/app/actions/AuctionForm.tsx
+++ frontend/web-app/app/actions/AuctionForm.tsx
@@ -3,9 +3,10 @@
 import { Button, TextInput } from 'flowbite-react';
 import React from 'react'
 import { FieldValue, FieldValues, useForm } from 'react-hook-form';
+import Input from '../components/input';
 
 export default function AuctionForm() {
-    const { register, handleSubmit, setFocus, formState: { isSubmitting, isValid, isDirty, errors } } = useForm();
+    const { control, handleSubmit, setFocus, formState: { isSubmitting, isValid, isDirty, errors } } = useForm();
 
     function onSubmit(data: FieldValues) {
         console.log(data);
@@ -13,23 +14,9 @@ export default function AuctionForm() {
 
     return (
         <form className='flex flex-col mt-3' onSubmit={handleSubmit(onSubmit)}>
-            <div className='mb-3 block'>
-                <TextInput
-                    {...register('make', { required: 'Make is required' })}
-                    placeholder='Make'
-                    color={errors?.make && 'failure'}
-                    helperText={errors.make?.message as string}
-                />
+            <Input label='Make' name='make' control={control} rules={{ required: 'Make is required' }} />
+            <Input label='Model' name='model' control={control} rules={{ required: 'Model is required' }} />
 
-            </div>
-            <div className='mb-3 block'>
-                <TextInput
-                    {...register('model', { required: 'Model is required' })}
-                    placeholder='Model'
-                    color={errors?.model && 'failure'}
-                    helperText={errors.model?.message as string}
-                />
-            </div>
             <div className='flex justify-between'>
                 <Button outline color='gray'>Cancel</Button>
                 <Button
