72ff368140ffd23a83c4b1816efbde93e1d7fdf8
“Vinicius <viniciusmv91@gmail.com>
2024-09-15 16:46:14 -0300

    Add auctions won and user selling auctions pages

--- frontend/web-app/app/nav/UserActions.tsx
+++ frontend/web-app/app/nav/UserActions.tsx
@@ -1,10 +1,11 @@
 'use client'
 
+import { useParamsStore } from '@/hooks/useParamsStore'
 import { Dropdown, DropdownDivider, DropdownItem } from 'flowbite-react'
 import { User } from 'next-auth'
 import { signOut } from 'next-auth/react' // signOut for client side component. Do not use: import { signOut } from '@/auth', it is a client side function 
 import Link from 'next/link'
-import { useRouter } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
 import React from 'react'
 import { AiFillCar, AiFillTrophy, AiOutlineLogout } from 'react-icons/ai'
 import { HiUser } from 'react-icons/hi'
@@ -16,18 +17,26 @@ type Props = {
 
 export default function UserActions({ user }: Props) {
     const router = useRouter();
+    const pathname = usePathname();
+    const setParams = useParamsStore(state => state.setParams);
+
+    function setWinner() {
+        setParams({winner: user.username, seller: undefined});
+        if(pathname !== '/') router.push('/');
+    }
+
+    function setSeller() {
+        setParams({seller: user.username, winner: undefined});
+        if(pathname !== '/') router.push('/');
+    }
 
     return (
         <Dropdown inline label={`Welcome ${user.name}`}>
-            <DropdownItem icon={HiUser}>
-                <Link href='/'>
-                    My Auctions
-                </Link>
+            <DropdownItem icon={HiUser} onClick={setSeller}>
+                My Auctions
             </DropdownItem>
-            <DropdownItem icon={AiFillTrophy}>
-                <Link href='/'>
+            <DropdownItem icon={AiFillTrophy} onClick={setWinner}>
                     Auctions won
-                </Link>
             </DropdownItem>
             <DropdownItem icon={AiFillCar}>
                 <Link href='/auctions/create'>
