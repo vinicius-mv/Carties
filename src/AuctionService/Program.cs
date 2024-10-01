8b07b2181ab5140f2071b3ee693023e9d98277d9
“Vinicius <viniciusmv91@gmail.com>
2024-09-30 22:54:06 -0300

    Add a gRPC server service

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,5 +1,6 @@
 using AuctionService.Consumers;
 using AuctionService.Data;
+using AuctionService.Services;
 using Contracts;
 using MassTransit;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -51,6 +52,8 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         options.TokenValidationParameters.NameClaimType = "username";
     });
 
+builder.Services.AddGrpc();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -59,6 +62,8 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapGrpcService<GrpcAuctionService>();
 
 try
 {
---
+++ src/AuctionService/Services/GrpcAuctionService.cs
@@ -0,0 +1,36 @@
+using AuctionService.Data;
+using Grpc.Core;
+
+namespace AuctionService.Services;
+
+public class GrpcAuctionService : GrpcAuction.GrpcAuctionBase
+{
+    private readonly AuctionDbContext _dbContext;
+
+    public GrpcAuctionService(AuctionDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public override async Task<GrpcAuctionResponse> GetAuction(GetAuctionRequest request, ServerCallContext context)
+    {
+        Console.WriteLine("==> Received Grpc request for auction");
+
+        var auction = await _dbContext.Auctions.FindAsync(Guid.Parse(request.Id));
+
+        if (auction == null) throw new RpcException(new Status(StatusCode.NotFound, $"Auction {request.Id} not found"));
+
+        var response = new GrpcAuctionResponse
+        {
+            Auction = new GrpcAuctionModel
+            {
+                AuctionEnd = auction.AuctionEnd.ToString(),
+                Id = auction.Id.ToString(),
+                ReservedPrice = auction.ReservePrice,
+                Seller = auction.Seller
+            }
+        };
+
+        return response;
+    }
+}
