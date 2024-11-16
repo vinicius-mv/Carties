160badaa9ea32be7eefa525746e0adb53a448210
“Vinicius <viniciusmv91@gmail.com>
2024-11-16 20:10:07 -0300

    Add AuctionControllerTests and update test project dependencies
    Introduce AuctionControllerTests to test AuctionsController.
    Import necessary namespaces for testing.
    Add private fields for mocks and instances in the test class.
    Initialize fields and configure AutoMapper in the constructor.
    Update AuctionService.UnitTests.csproj with AutoFixture and Moq.

---
+++ tests/AuctionService.UnitTests/AuctionControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using AuctionService.Controllers;
+using AuctionService.Data;
+using AuctionService.RequestHelpers;
+using AutoFixture;
+using AutoMapper;
+using MassTransit;
+using Moq;
+
+namespace AuctionService.UnitTests;
+
+public class AuctionControllerTests
+{
+    private readonly Mock<IAuctionRepository> _auctionRepo;
+    private readonly Mock<IPublishEndpoint> _publishEndpoint;
+    private readonly Fixture _fixture;
+    private readonly AuctionsController _controller;
+    private readonly IMapper _mapper;
+
+    public AuctionControllerTests()
+    {
+        _fixture = new Fixture();
+        _auctionRepo = new Mock<IAuctionRepository>();
+        _publishEndpoint = new Mock<IPublishEndpoint>();
+
+        var mockMapper = new MapperConfiguration(mc =>
+        {
+            mc.AddMaps(typeof(MappingProfiles).Assembly);
+        }).CreateMapper().ConfigurationProvider;
+        _mapper = new Mapper(mockMapper);
+        _controller = new AuctionsController(_auctionRepo.Object, _mapper, _publishEndpoint.Object);
+    }
+
+
+}
